fa392d33c62e02faddfb3a1d9e87904e045c72b5
ZeN220 <artaganmalsagov07@gmail.com>
2022-01-07 17:58:09 +0300

    add command .msgid

--- commands/dev.py
+++ commands/dev.py
@@ -48,6 +48,29 @@ async def user_id_from_msg(event: SimpleUserEvent):
 
 @simple_user_message_handler(
     dev_router,
+    TextFilter('.msgid')
+)
+async def get_message_id(event: SimpleUserEvent):
+    reply = event.object.object.extra_message_data.get('reply')
+    fwd = event.object.object.extra_message_data.get('fwd')
+    message_id = (await event.api_ctx.messages.get_by_id(
+        message_ids=event.object.object.message_id
+    )).response.items[0]
+
+    if reply:
+        message_id = message_id.reply_message.id
+    elif fwd:
+        message_id = message_id.fwd_messages[0].id
+
+    await bot.api_context.messages.send(
+        message=f'MessageID -- {message_id}',
+        peer_id=my_id,
+        random_id=0
+    )
+
+
+@simple_user_message_handler(
+    dev_router,
     TextStartswithFilter(('.eval', '.евал'))
 )
 async def run_eval(event: SimpleUserEvent):
@@ -68,10 +91,10 @@ async def run_eval(event: SimpleUserEvent):
     TextFilter(('.ping', '.пинг'))
 )
 async def ping(event: SimpleUserEvent):
-    message_id = event.object.object.message_id
+    msg_id = event.object.object.message_id
     before_time = time()
     await event.api_ctx.messages.delete(
-        message_ids=message_id,
+        message_ids=msg_id,
         delete_for_all=1
     )
     after_time = time()
