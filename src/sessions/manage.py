acf63e3ab0368c8f59081baf21843f5b3d328e5d
ZeN220 <artaganmalsagov07@gmail.com>
2022-07-10 16:40:27 +0300

    added classes to work with sessions

---
+++ src/sessions/manage.py
@@ -0,0 +1,39 @@
+from typing import List, Optional
+import logging
+
+from .session import Session
+
+logger = logging.getLogger(__name__)
+
+
+class SessionManager:
+    main_session: Optional[Session] = None
+    sessions: List[Session] = []
+
+    @classmethod
+    def is_duplicate(cls, other_session: Session) -> bool:
+        for session in cls.sessions:
+            if session == other_session:
+                return True
+        return False
+
+    @classmethod
+    def add_session(cls, session: Session) -> None:
+        duplicate = cls.is_duplicate(session)
+        if duplicate:
+            logger.warning(f'Сессия [{session.owner_id}] игнорируется из-за имеющегося дубликата')
+            return
+
+        if not session.is_main:
+            cls.sessions.append(session)
+            logger.info(f'Сессия [{session.owner_id}] успешно инициализирована.')
+            return
+        elif session.is_main and not cls.main_session:
+            cls.main_session = session
+            logger.info(f'Сессия [{session.owner_id}] успешно инициализирована как основная.')
+            return
+
+        logger.info(
+            f'Основная сессия [{cls.main_session.owner_id}] уже инициализирована, '
+            f'поэтому [{session.owner_id}] игнорируется.'
+        )
---
+++ src/sessions/session.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+
+from vkwave.api.token.token import UserSyncSingleToken, BotSyncSingleToken, Token
+from vkwave.api import APIOptionsRequestContext, API
+
+
+@dataclass
+class User:
+    owner_id: int
+    token: str
+    api_context: APIOptionsRequestContext
+
+    @classmethod
+    async def create_from_token(cls, user_token: str) -> 'User':
+        token = UserSyncSingleToken(Token(user_token))
+        api_context = API(tokens=token).get_context()
+
+        user_profile = await api_context.users.get()
+        owner_id = user_profile.response[0].id
+
+        return cls(
+            owner_id=owner_id,
+            token=user_token,
+            api_context=api_context
+        )
+
+
+@dataclass
+class Group:
+    api_context: APIOptionsRequestContext
+
+    @classmethod
+    def create_from_token(cls, bot_token: str) -> 'Group':
+        token = BotSyncSingleToken(Token(bot_token))
+        api_context = API(tokens=token).get_context()
+        return cls(api_context=api_context)
+
+
+@dataclass
+class Session:
+    user: User
+    group: Group
+    commands_prefix: str
+    is_main: bool
+
+    @classmethod
+    async def create_from_tokens(
+        cls,
+        user_token: str,
+        bot_token: str,
+        commands_prefix: str,
+        is_main: bool
+    ) -> 'Session':
+        user = await User.create_from_token(user_token)
+        group = Group.create_from_token(bot_token)
+        return cls(user=user, group=group, commands_prefix=commands_prefix, is_main=is_main)
+
+    @property
+    def owner_id(self) -> int:
+        return self.user.owner_id
+
+    def __eq__(self, other_session: 'Session') -> bool:
+        return self.user.token == other_session.user.token
