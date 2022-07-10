74cf616de826b176d4fb86e792084cee2212e9f5
ZeN220 <artaganmalsagov07@gmail.com>
2022-07-10 18:30:30 +0300

    rename method '__getattribute__' to '__getattr__'

--- src/config/parser.py
+++ src/config/parser.py
@@ -2,7 +2,14 @@ import toml
 
 
 class Config(dict):
-    def __getattribute__(self, item):
+    def __init__(self, config_dict: dict):
+        super().__init__()
+        for key, value in config_dict.items():
+            if isinstance(value, dict):
+                value = Config(value)
+            self[key] = value
+
+    def __getattr__(self, item):
         return super().__getitem__(item)
 
     def __setattr__(self, key, value):
@@ -14,5 +21,5 @@ class Config(dict):
     @classmethod
     def load_from_file(cls, filename: str) -> 'Config':
         with open(filename, encoding='utf-8') as raw_config:
-            config = toml.load(raw_config)
-            return cls(**config)
+            raw_config = toml.load(raw_config)
+            return cls(raw_config)
