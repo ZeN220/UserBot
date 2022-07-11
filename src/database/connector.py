f1db7cd65c129a305790d30731bd29dac24ba258
ZeN220 <artaganmalsagov07@gmail.com>
2022-07-11 16:51:49 +0300

    added logging and try/except for DBConnectionError

--- src/database/connector.py
+++ src/database/connector.py
@@ -1,9 +1,18 @@
+import logging
+
+from tortoise.exceptions import DBConnectionError
 from tortoise import Tortoise
+
+logger = logging.getLogger(__name__)
 
 
 async def connect_database(database_url: str):
-    await Tortoise.init(
-        db_url=database_url,
-        modules={'models': ['src.database.models']}
-    )
-    await Tortoise.generate_schemas()
+    try:
+        await Tortoise.init(
+            db_url=database_url,
+            modules={'models': ['src.database.models']}
+        )
+        await Tortoise.generate_schemas()
+        logger.debug(f'База данных {database_url} успешно подключена.')
+    except DBConnectionError:
+        logger.error(f'При попытке подключения к {database_url} произошла ошибка.')
