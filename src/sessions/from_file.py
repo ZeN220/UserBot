8ac159ac82be5b02ad3a745338ca2dfdf312c837
ZeN220 <artaganmalsagov07@gmail.com>
2022-07-24 18:59:49 +0300

    refactoring comment

--- src/sessions/from_file.py
+++ src/sessions/from_file.py
@@ -8,7 +8,10 @@ class SessionsFile:
 
     def get_sessions(self) -> List[dict]:
         sessions = toml.load(self.file)['sessions']
-        # При отсутствии сессий в sessions будет лежать [{}], из за этого нужно проверять длину списка и возвращать пустой список
+        """
+        При отсутствии сессий в sessions будет лежать [{}], 
+        из за этого нужно проверять длину списка и возвращать пустой список
+        """
         if len(sessions) == 1 and not sessions[0]:
             return []
         return sessions
