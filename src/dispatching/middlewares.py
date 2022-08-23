79ae3af98e696e29cedb8a428a4861b3cd90853f
ZeN <hidden@gmail.com>
2022-08-23 22:56:25 +0300

    remove unwanted middlewares

--- src/dispatching/middlewares.py
+++ src/dispatching/middlewares.py
@@ -1,28 +1,15 @@
-from typing import List
-
-from vkwave.types.user_events import MessageFlag
 from vkwave.bots import MiddlewareResult, BaseMiddleware
+from sqlalchemy.orm import sessionmaker
 
 from src.dispatching import UserEvent
-from src.database import Template
+from src.services import HolderGateway
 
 
-class MessageMiddleware(BaseMiddleware):
-    def __init__(self, event_ids: List[int]):
-        self.events_ids = event_ids
+class DatabaseMiddleware(BaseMiddleware):
+    def __init__(self, session_pool: sessionmaker):
+        self.session_pool = session_pool
 
     async def pre_process_event(self, event: UserEvent) -> MiddlewareResult:
-        if event.object is None:
-            return MiddlewareResult(False)
-        event_id = event.object.object.event_id
-        return MiddlewareResult(event_id in self.events_ids)
-
-
-class FromMeMiddleware(BaseMiddleware):
-    async def pre_process_event(self, event: UserEvent) -> MiddlewareResult:
-        # В списке флагов события последним элементом является сумма всех флагов, поэтому тут используется битовое "И"
-        is_from_me = event.object.object.flags[-1] & MessageFlag.OUTBOX.value
-        return MiddlewareResult(bool(is_from_me))
-
-
-MIDDLEWARES = [MessageMiddleware([2, 4, 5]), FromMeMiddleware()]
+        session_pool = self.session_pool()
+        event['gateway'] = HolderGateway(session_pool)
+        return MiddlewareResult(True)
