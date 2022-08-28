ccc0221df55c6b17586eb4f5008bb80d3bdfb139
ZeN <artaganmalsagov07@gmail.com>
2022-08-29 00:06:55 +0300

    added supported audio messages in templates & add 'session' to DI

--- src/routers/new_message.py
+++ src/routers/new_message.py
@@ -1,4 +1,5 @@
 from vkwave.bots import DefaultRouter, FromMeFilter
+import re
 
 from src.commands.base.manage import CommandManager
 from src.commands.base.errors import NotEnoughArgs
@@ -9,23 +10,35 @@ from src.dispatching.filters import TemplateFilter, PrefixFilter, EventTypeFilte
 new_message_router = DefaultRouter(
     [EventTypeFilter(4), FromMeFilter(True)]
 )
+AUDIO_DOCUMENT_REGEXP = re.compile(r'doc\d+_\d+_\w+')
 
 
 @new_message_router.registrar.with_decorator(TemplateFilter())
 async def send_template(event: UserEvent):
+    api_context = event.session.user.api_context
+    message_id = event.object.object.message_id
+    peer_id = event.object.object.peer_id
     template = event['template']
-    await event.session.user.api_context.messages.edit(
-        message_id=event.object.object.message_id, peer_id=event.object.object.peer_id,
+
+    if template['attachment']:
+        is_audio = AUDIO_DOCUMENT_REGEXP.match(template['attachment'][0])
+        if is_audio:
+            await api_context.messages.delete(message_ids=message_id, delete_for_all=1)
+            await api_context.messages.send(peer_id=peer_id, **template, random_id=0)
+            return
+
+    await api_context.messages.edit(
+        message_id=message_id, peer_id=peer_id,
         keep_forward_messages=1, **template
     )
 
 
 @new_message_router.registrar.with_decorator(PrefixFilter())
 async def execute_command(event: UserEvent):
-    text = event.object.object.text[1:].lstrip()
+    event.object.object.text = event.object.object.text[1:].lstrip()
     command = await CommandManager.find_command(
         session=event.session,
-        text=text
+        text=event.object.object.text
     )
     if command is None:
         return
@@ -40,7 +53,7 @@ async def execute_command(event: UserEvent):
     #  ее стоит обрабатывать на более низком уровне
     try:
         response = await command.start(
-            event, gateway=event['gateway'], api_context=event.api_ctx
+            event, gateway=event['gateway'], api_context=event.api_ctx, session=event.session
         )
     except NotEnoughArgs:
         await event.session.send_service_message(
