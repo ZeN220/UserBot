f626c82f9f9792798e78aaab3202f39f21a26252
ZeN <artaganmalsagov07@gmail.com>
2022-08-29 12:11:40 +0300

    added commands from templates module to __init__.py

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,4 +1,4 @@
 from .base import CommandResponse
 from .dialogs import ReadDialogsHandler
 from .social import AddFriendHandler, RemoveBlockHandler, RemoveFriendHandler, AddBlockHandler
-from .templates import AddTemplateHandler
+from .templates import AddTemplateHandler, GetTemplatesHandler, DeleteTemplateHandler
