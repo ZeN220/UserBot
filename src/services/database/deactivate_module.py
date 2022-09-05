c9643887a7f158cc931ff50c5e4155d60087c2be
ZeN <artaganmalsagov07@gmail.com>
2022-09-05 16:45:50 +0300

    added 'DeactivateModule' service

---
+++ src/services/database/deactivate_module.py
@@ -0,0 +1,23 @@
+from sqlalchemy import delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import DeactivateModule
+from .base import BaseGateway
+
+
+class DeactivateModuleGateway(BaseGateway[DeactivateModule]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(DeactivateModule, session)
+
+    async def delete_by_owner_id_and_name(self, module: str, owner_id: int) -> None:
+        query = delete(DeactivateModule).where(
+            (DeactivateModule.module == module) & (DeactivateModule.session_owner_id == owner_id)
+        )
+        async with self.session.begin():
+            await self.session.execute(query)
+
+    async def create(self, module: str, owner_id: int) -> DeactivateModule:
+        async with self.session.begin():
+            model = DeactivateModule(module=module, session_owner_id=owner_id)
+            self.save(model)
+        return model
--- src/services/database/holder.py
+++ src/services/database/holder.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from .session import SessionGateway
 from .template import TemplateGateway
+from .deactivate_module import DeactivateModuleGateway
 
 
 @dataclass
@@ -11,7 +12,9 @@ class HolderGateway:
     database_session: AsyncSession
     template: TemplateGateway = field(init=False)
     session: SessionGateway = field(init=False)
+    deactivate_module: DeactivateModuleGateway = field(init=False)
 
     def __post_init__(self):
         self.template = TemplateGateway(self.database_session)
         self.session = SessionGateway(self.database_session)
+        self.deactivate_module = DeactivateModuleGateway(self.database_session)
